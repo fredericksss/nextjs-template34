774b2763a8cff848273be6bab695334749d27785
fredericksss <132380303+fredericksss@users.noreply.github.com>
2024-05-10 19:34:25 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          first change &nbsp;
+          second change &nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
